c97f997cf44f9ef10fb082acfe73a6df2b1220c8
AldoRD <aramirezd1501@alumno.ipn.mx>
2022-01-25 22:10:48 -0600

    Update

--- settings/keys.py
+++ settings/keys.py
@@ -12,8 +12,10 @@ keys = [
         ([mod], "k", lazy.layout.up()),
         ([mod], "h", lazy.layout.left()),
         ([mod], "l", lazy.layout.right()),
+
         # Toggle floating
         ([mod, "shift"], "f", lazy.window.toggle_floating()),
+
         # Move windows between left/right columns or move up/down in current stack.
         # Moving out of range in Columns layout will create new column.
         ([mod, "shift"], "h", lazy.layout.shuffle_left()),
@@ -27,37 +29,52 @@ keys = [
         # ([mod, "control"], "l", lazy.layout.grow_right()),
         # ([mod, "control"], "j", lazy.layout.grow_down()),
         # ([mod, "control"], "k", lazy.layout.grow_up()),
+        
         # Change window sizes (MonadTall)
         ([mod, "control"], "l", lazy.layout.grow()),
         ([mod, "control"], "h", lazy.layout.shrink()),
+       
         # Toggle between different layouts as defined below
         ([mod], "Tab", lazy.next_layout()),
         ([mod, "shift"], "Tab", lazy.prev_layout()),
+        
         # Kill window
         ([mod], "w", lazy.window.kill()),
+        
         # Switch focus of monitors
         ([mod], "period", lazy.next_screen()),
         ([mod], "comma", lazy.prev_screen()),
+        
         # Restart Qtile
         ([mod, "control"], "r", lazy.restart()),
         ([mod, "control"], "q", lazy.shutdown()),
         ([mod], "r", lazy.spawncmd()),
+        
         # ------App Configs----------
         # Menu
         ([mod], "m", lazy.spawn("open_launcher menu")),
+        
         # Window Nav
         ([mod, "shift"], "m", lazy.spawn("rofi -m -1 -show")),
+        
         # Browser
         ([mod], "b", lazy.spawn("firefox")),
+        
         # File Explorer
         ([mod], "e", lazy.spawn("kitty -e ranger")),
+        
         # Terminal
         ([mod], "Return", lazy.spawn("kitty")),
+        
         # Screenshot
         ([mod], "s", lazy.spawn("screenshot")),
         ([mod, "shift"], "s", lazy.spawn("screenshot window")),
         ([mod, "control", "shift"], "s", lazy.spawn("screenshot select")),
+
+        # Extra menus
         ([mod, "mod1"], "l", lazy.spawn("open_launcher power")),
+        ([mod ,"mod1"], "m", lazy.spawn("open_launcher music")), 
+        
         # ------------ Hardware Configs ------------
         # Volume
         # ([], "XF86AudioLowerVolume", lazy.spawn("pamixer --decrease 5")),
@@ -66,6 +83,7 @@ keys = [
         ([], "XF86AudioLowerVolume", lazy.spawn("changevolume down")),
         ([], "XF86AudioRaiseVolume", lazy.spawn("changevolume up")),
         ([], "XF86AudioMute", lazy.spawn("changevolume mute")),
+        
         # Brightness
         ([], "XF86MonBrightnessUp", lazy.spawn("changebright up")),
         ([], "XF86MonBrightnessDown", lazy.spawn("changebright down")),
